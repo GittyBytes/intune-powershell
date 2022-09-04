bb5786ffbbb9b22cfc94852db7e061a9d0b335ce
GittyBytes <48442734+GittyBytes@users.noreply.github.com>
2022-09-04 22:54:10 +0200

    Update is-winget-installed.ps1
    Some tweaks

--- is-winget-installed.ps1
+++ is-winget-installed.ps1
@@ -1,8 +1,22 @@
-if (Get-AppxPackage -Name Microsoft.Winget.Source) {
+if (Get-AppxPackage -Name Microsoft.DesktopAppInstaller) {
 
-return $true;
+  return $true;
 
 }else{
+  $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 
-return $false;
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  $releases = Invoke-RestMethod -uri "$($releases_url)"
+  $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
+  Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+  Add-AppxPackage -Path "http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/e700ed37-7b26-4e1d-b5e2-ba28047c6074?P1=1662324695&P2=404&P3=2&P4=Tti5OaWtsRyO1eiXVKNu6X0li3RqeqelhT4YjS21VM4FwUT6YyiGtnZJsFoKnonXn%2frkq7uzTIIBKspX8Esd6A%3d%3d"	
+  Add-AppxPackage -Path $latestRelease.browser_download_url
+  if (Get-AppxPackage -Name Microsoft.DesktopAppInstaller) {
+
+  return $true;
+  }else{
+
+  return $false;
+  }
+
 }
