0470377ad79032dc44551000d6d9cce4bb22eedd
GittyBytes <48442734+GittyBytes@users.noreply.github.com>
2022-09-07 00:50:45 +0200

    DS
    Adding winget installation and custom detection script

---
+++ detect-winget-app-install.ps1
@@ -0,0 +1,31 @@
+﻿$Apptodetect = "Notepad++.Notepad++"
+
+<# FUNCTIONS #>
+
+#Initialization
+function Start-Init {
+
+    #Config console output encoding
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+}
+
+function Confirm_Winget_Install {
+    if (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}) {
+        return $true
+    }
+    else {
+        write-host "winget is not installed"
+        return $false
+    }
+}
+
+
+#Run Init Function
+Start-Init
+
+if (Confirm_Winget_Install) {
+    $InstalledApp = & winget list --Id $AppToDetect --accept-source-agreements | Out-String
+    if ($InstalledApp -match [regex]::Escape($AppToDetect)) {
+    return "$AppToDetect is installed!"
+    }
+}
