f3331dd75f85248325856fb56d860717ecbc3c2d
GittyBytes <48442734+GittyBytes@users.noreply.github.com>
2022-09-08 14:49:24 +0200

    testing

--- install-winget-app.ps1
+++ install-winget-app.ps1
@@ -12,21 +12,21 @@ function Start-Init {
     [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 }
 
-function Confirm_Winget_Install {
-    if (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}) {
-        return $true
-    }
-    else {
-        write-host "winget is not installed"
-        exit 1
-    }
-}
+#function Confirm_Winget_Install {
+#    if (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}) {
+#        return $true
+#    }
+#    else {
+#        write-host "winget is not installed"
+#        exit 1
+#    }
+#}
 
 
 #Run Init Function
 Start-Init
 
-if (Confirm_Winget_Install) {
+#if (Confirm_Winget_Install) {
     $InstalledApp = & winget list --Id $AppID --accept-source-agreements | Out-String
     if (!($InstalledApp -match [regex]::Escape($AppID))) {
         write-host "$AppID is not installed, starting installation"
@@ -41,4 +41,4 @@ if (Confirm_Winget_Install) {
     write-host "$AppID is not succesfully installed!"
     exit 1
     }
-}
+#}
