f5aab23a709d107978c87fa876e0ef7ded0233fd
GittyBytes <48442734+GittyBytes@users.noreply.github.com>
2022-09-09 00:07:52 +0200

    Update detect-winget-app-install.ps1

--- detect-winget-app-install.ps1
+++ detect-winget-app-install.ps1
@@ -1,34 +1,10 @@
-﻿$Apptodetect = "Notepad++.Notepad++"
+﻿$Apptodetect = "7Zip.7Zip"
 
-<# FUNCTIONS #>
-
-#Initialization
-function Start-Init {
-
-    #Config console output encoding
-    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-}
-
-function Confirm_Winget_Install {
-    if (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}) {
-        return $true
-    }
-    else {
-        write-host "winget is not installed"
-        exit 1
-    }
-}
-
-
-#Run Init Function
-Start-Init
-
-if (Confirm_Winget_Install) {
-    $InstalledApp = & winget list --Id $AppToDetect --accept-source-agreements | Out-String
-    if ($InstalledApp -match [regex]::Escape($AppToDetect)) {
+$InstalledApp = $(winget list --Id $AppToDetect --accept-source-agreements | Out-String)
+if ($InstalledApp -match [regex]::Escape($AppToDetect)) {
     return "$AppToDetect is installed!"
+    exit 0
     }else {
     write-host "$AppToDetect is not installed!"
     exit 1
-    }
 }
